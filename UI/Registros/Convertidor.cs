3dd3b95fb2779509581ccc3830e61b57fbfc5064
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-03-16 20:51:25 -0400

    Botones Funcionando

---
+++ UI/Registros/Convertidor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2doParcial.UI.Registros
+{
+   public static class Convertidor
+    {
+        public static int ToInt(this string valor)
+        {
+            int retorno = 0;
+            int.TryParse(valor, out retorno);
+            return retorno;
+        }
+    }
+}
--- UI/Registros/rRegistro.xaml.cs
+++ UI/Registros/rRegistro.xaml.cs
@@ -40,7 +40,23 @@ namespace _2doParcial.UI.Registros
 
         }
 
+        private Llamadas LlenaClase()
+        {
+            Llamadas llamadas = new Llamadas();
+            llamadas.LlamadaId = Convert.ToInt32(IdTextBox.Text);
+            llamadas.Descripcion = DescripcionTextBox.Text;
 
+            llamadas.Detalles = this.Detalles;
+
+            return llamadas;
+        }
+        private void LlenaCampo(Llamadas llamadas)
+        {
+            IdTextBox.Text = Convert.ToString(llamadas.LlamadaId);
+            DescripcionTextBox.Text = llamadas.Descripcion;
+            this.Detalles = llamadas.Detalles;
+            CargarGrid();
+        }
 
 
         private void CargarGrid()
@@ -49,5 +65,124 @@ namespace _2doParcial.UI.Registros
             DetalleDataGrid.ItemsSource = this.Detalles;
         }
 
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Llamadas llamadas;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            llamadas = LlenaClase ();
+            Limpiar();
+
+            //Determinar si es guardar o modificar
+
+            if (string.IsNullOrWhiteSpace(IdTextBox.Text) || IdTextBox.Text == "0")
+                paso = LlamadasBLL.Guardar(llamadas);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar una persona que no existe", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                paso = LlamadasBLL.Modificar(llamadas);
+            }
+
+            //Informar el resultado
+            if (paso)
+                MessageBox.Show("Guardado!!", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("No fue posible guardar!!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Llamadas llamadas = LlamadasBLL.Buscar((int)IdTextBox.Text.ToInt());
+           return (llamadas != null);
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (string.IsNullOrEmpty(DescripcionTextBox.Text))
+            {
+                MessageBox.Show("EL campo Descripcion no puede estar vacio", "Aviso", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                DescripcionTextBox.Focus();
+                paso = false;
+            }
+            if (this.Detalles.Count == 0)
+            {
+                MessageBox.Show("Debe agregar una Llamada", "Aviso", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                ProblemaTextBox.Focus();
+                SolucionTextBox.Focus();
+                paso = false;
+            }
+            return paso;
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DetalleDataGrid.ItemsSource != null)
+            {
+                this.Detalles = (List<LlamadasDetalle>)DetalleDataGrid.ItemsSource;
+            }
+
+            //Agregar un nuevo detalle con los datos introducidos
+            this.Detalles.Add(new LlamadasDetalle
+            {
+                id = IdTextBox.Text.ToInt(),
+                Problema = ProblemaTextBox.Text,
+                Solucion = SolucionTextBox.Text,
+                
+
+
+            });
+            CargarGrid();
+            ProblemaTextBox.Focus();
+            ProblemaTextBox.Clear();
+            SolucionTextBox.Clear();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Llamadas llamadaAnterior = LlamadasBLL.Buscar(llamadas.LlamadaId);
+
+            if (llamadaAnterior != null)
+            {
+                llamadas = llamadaAnterior;
+                Refrescar();
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("Llamada no encontrada");
+            }
+        }
+
+        private void RemoverButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DetalleDataGrid.Items.Count > 0 && DetalleDataGrid.SelectedItem != null)
+            {
+                //remover la fila
+                Detalles.RemoveAt(DetalleDataGrid.SelectedIndex);
+                CargarGrid();
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            int.TryParse(IdTextBox.Text, out id);
+
+            Limpiar();
+
+            if (LlamadasBLL.Eliminar(id))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show(IdTextBox.Text, "No se puede eliminar una llamada que no existe");
+        }
     }
 }
